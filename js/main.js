3bebe54255ac6d1b4b2b8683096f0176c9f28b42
mt <markusT@mt-MacBook-Pro.local>
2014-07-09 14:02:14 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var fileName;
+
+function init()
+{
+	fileName = document.getElementById("fileName").value;
+}
+
+function createFile(text)
+{
+	var txtFile = new XMLHttpRequest();
+		txtFile.open("GET", "php/createFile.php?name="+fileName+"&text="+document.getElementById("outputText").value, true);
+		txtFile.onreadystatechange = function(data) {
+  	if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (txtFile.status === 200) {  // Makes sure it's found the file.
+    }
+  }
+}
+
+        txtFile.send(null);
+}
+
+function load()
+{
+    console.log("load File ->  " + fileName)
+
+	var txtFile = new XMLHttpRequest();
+		txtFile.open("GET", "json/"+fileName, true);
+		txtFile.onreadystatechange = function() {
+  	if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (txtFile.status === 200) {  // Makes sure it's found the file.
+      		allText = txtFile.responseText;
+      		lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+    		// document.getElementById("highscoreTable").innerHTML = lines;
+     		// alert(lines.length)
+		document.getElementById("outputText").value = lines;
+//    	 for(i = 0; i < lines.length; i++)//Add a </p> element for each line
+//     	{
+//     		//alert(lines[i])
+//     		var node = document.createElement("p");
+//     			node.innerHTML = i+" "+lines[i];
+//
+//			console.log(node);
+//
+//		}
+    }
+  }
+}
+txtFile.send(null);
+}
+
+
+function save()
+{
+//    alert(document.getElementsByTagName("textarea")[0].value)
+
+    createFile();
+
+	console.log("save File ->  " + fileName)
+
+    var animal = {name: document.getElementsByTagName("textarea")[0].value};
+    var json = JSON.stringify(animal); //json is a JSON string
+}
---
+++ php/createFile.php
@@ -0,0 +1,17 @@
+<?
+$title=htmlspecialchars($_GET['text']);
+$title=stripslashes($title);
+$name=htmlspecialchars($_GET['name']);
+$name=stripslashes($name);
+$score=(int)$_GET['score'];
+
+//echo "<span style='color:red'>Welcome <b>$name</b> to JavaScript Kit. So you're <b>$score</b> years old eh?</span>";
+//echo "<span style='color:red'>Welcome <b>$name</b> to JavaScript Kit. So you're <b>$score</b> years old eh?</span>";
+
+//$myFile = "../json/".$name.".json";
+$myFile = "../json/".$name;
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = " \n$title  ";
+fwrite($fh, $stringData);
+fclose($fh);
+?>
